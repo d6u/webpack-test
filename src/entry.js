53aedd65c763705c9190007b32608175d103c26c
Daiwei <daiweilu123@gmail.com>
2015-06-09 23:50:51 -0400

    Make local css work

--- src/entry.js
+++ src/entry.js
@@ -1,38 +1,10 @@
-import style from './style.css';
-import content from './content.js';
 import React from 'react';
-
-class Test {
-  write() {
-    document.write(content);
-  }
-}
-
-new Test().write();
-
-function* gen() {
-  yield 1;
-  yield 2;
-}
-
-var g = gen();
-
-console.log(g.next());
-console.log(g.next());
-console.log(g.next());
-
-async function run() {
-  let a = await Promise.resolve(123);
-  let b = await Promise.resolve(123);
-  console.log(123);
-}
-
-run();
+import style from './style.scss';
 
 class HelloMessage extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return <div className={style['hello-world']}>Hello {this.props.name}</div>;
   }
 }
 
-React.render(<HelloMessage name="John" />, document.getElementById('main'));
+React.render(<HelloMessage name='John' />, document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
 'use strict';
 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './src/entry.js',
   devtool: 'source-map',
   output: {
     path: './public',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
 
   module: {
-    loaders: [{
-      test: /\.css$/,
-      loader: 'style!css'
-    }, {
-      test: /\.(js|jsx)$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel',
-      query: {
-        optional: ['runtime', 'es7.asyncFunctions']
-      }
-    }]
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          [
+            'css-loader?localIdentName=[local]__[hash:base64:5]',
+            'postcss-loader',
+            'sass-loader?outputStyle=expanded'
+          ].join('!'),
+        ),
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          optional: ['runtime', 'es7.asyncFunctions'],
+        },
+      },
+    ],
   },
+
+  postcss: [
+    require('autoprefixer-core'),
+    require('postcss-local-scope'),
+  ],
+
+  plugins: [
+    new ExtractTextPlugin('style.css', {allChunks: true}),
+  ],
 };
